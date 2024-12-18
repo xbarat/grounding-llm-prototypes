6f814aa285ed1f2ecc46521b618fab0425f3ab61
barath <barat.paim@gmail.com>
2024-12-18 16:34:59 -0500

    FRONTEND WORKS: Imported Global.css to Layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import './globals.css'
 
 import { Sidebar } from '@/components/sidebar'
 import { ThemeProvider } from '@/components/theme-provider'
